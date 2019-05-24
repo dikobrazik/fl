6520933ec09f07c0d11ed2427e4890d1b55d2ca3
dikobrazik <ilnar.khusaenov@gmail.com>
2019-05-24 18:12:46 +0300

    0.5

--- js/scripts.js
+++ js/scripts.js
@@ -16,16 +16,44 @@ function connectDB(f){
         connectDB(f);
     }
 }
-
-function get(index){
-    connectDB(function(index){
-        var index = objectStore.index("name");
-        index.get("Donna").onsuccess = function(event) {
-            alert("Donna's SSN is " + event.target.result.ssn);
-        };
+/**
+ * 
+ * @param {number} id - id пациента в бд
+ * @return {Object} - возвращает данные о пациенте 
+ */
+function get(id){
+    connectDB(function(db){
+      var request = db.transaction([storeName], "readwrite")
+                  .objectStore(storeName).get(Number(id));
+      request.onsuccess = function(event) {
+        fillAllFields(event.currentTarget.result)
+      };
+      // var objectStore = db.transaction(storeName).objectStore(storeName);
+      // objectStore.openCursor().onsuccess = function(event) {
+      //   var cursor = event.target.result;
+      //   if (cursor) {
+      //     if(cursor.key == id) return fillAllFields(cursor.value);
+      //     cursor.continue();
+      //   }
+      //   else {
+      //     alert("Not found!");
+      //   }
+      // };
     })
 }
-
+function remove(id){
+  connectDB(function(db){
+    var request = db.transaction([storeName], "readwrite")
+                  .objectStore(storeName).delete(id);
+    request.onsuccess = function(event) {
+      if(event.type == 'success') alert('Успешно')
+      else alert('Возникла проблема...')
+    };
+    request.onerror = function(event) {
+      alert(event)
+    };
+  })
+}
 function list(f){
     connectDB(function(db){
 		var rows = [],
@@ -50,7 +78,7 @@ function list(f){
 }
 function add(data){
     connectDB(function(db){
-		var request = db.transaction([storeName], "readwrite").objectStore(storeName).put(data);
+    var request = db.transaction([storeName], "readwrite").objectStore(storeName).put(data);
 		request.onsuccess = function(){
 			return request.result;
 		}
@@ -62,10 +90,7 @@ var generalInformation = `<div class="input-group mb-3 p-0">
   <button class="btn btn-outline-secondary dropdown-toggle col rounded" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Показатель</button>
   <div class="dropdown-menu col p-0 rounded" id="myDropdown">
   <input type="text" id="searchInput" class="form-control p-2 searchInput rounded" onkeyup="filterFunction(this)" aria-label="Text input with dropdown button" placeholder="Search..">
-    <a class="dropdown-item" onclick="btnName(this)">Дата госп-и</a>
     <a class="dropdown-item" onclick="btnName(this)">койко-день</a>
-    <a class="dropdown-item" onclick="btnName(this)">возраст</a>
-    <a class="dropdown-item" onclick="btnName(this)">пол</a>
     <a class="dropdown-item" onclick="btnName(this)">Диагноз ЛОП/СОП/ТОП</a>
     <a class="dropdown-item" onclick="btnName(this)">диагноз ОП/ИПН/СПН/ХП</a>
     <a class="dropdown-item" onclick="btnName(this)">осложнения</a>
@@ -112,7 +137,7 @@ var generalInformation = `<div class="input-group mb-3 p-0">
     <a class="dropdown-item" onclick="btnName(this)">ОЦЕНКА</a>
   </div>
 </div>
-<input type="text" class="form-control" aria-label="Text input with dropdown button">
+<input type="text" class="form-control added-data-field" aria-label="Text input with dropdown button">
 
 </div>`;
 
@@ -181,7 +206,7 @@ var generalPeriodInformation =
 <a class="dropdown-item" onclick="btnName(this)">PROMISE</a>
 </div>
 </div>
-<input type="text" class="form-control" aria-label="Text input with dropdown button">
+<input type="text" class="form-control added-data-field" aria-label="Text input with dropdown button">
 
 </div>`;
 function filterFunction(data) {
@@ -200,4 +225,21 @@ function filterFunction(data) {
       }
     }
   }
-// var generalInformationArray = ['Дата госп-и', 'койко-день', 'возраст', 'пол', 'Диагноз ЛОП/СОП/ТОП', 'диагноз ОП/ИПН/СПН/ХП', 'осложнения', 'Вид операции', 'сопутств.заб.', 'этиология', 'исход', 'Ranson', 'ССВР', 'None', 'М-APACHE II', 'к/день в ОАРИТ', '№сутки в ОАРИТ', 'нач.заб.,час', 'тип госпитализации', 'Сутки перевода', '№ госп-и', 'ранн.опер.', 'расп.боли', 'нарк.анальг.', 'мн.рвота', 'рв.коф.гущ.', 'BMI', 'c-м Грея-К.', 'Группа крови', 'Rh', 'ЭКГ', 'None', 'эффект от леч. 24 часа', 'антибак.проф.', 'антиферм.', 'антисекр.преп.', 'гемосорб.', 'СДК', 'нутр.подд-ка.', 'дрен.УЗ-контр', 'лап.(леч.)', 'ЭПСТ', 'ПКТ', 'ТИАБ(диагн)', 'миниинваз.оп.', 'лапаротомий-всего', 'Примечание', 'ОЦЕНКА',]
+var generalInformationArray = ['Дата госп-и', 'койко-день', 'возраст', 'пол', 'Диагноз ЛОП/СОП/ТОП', 'диагноз ОП/ИПН/СПН/ХП', 'осложнения', 'Вид операции', 'сопутств.заб.', 'этиология', 'исход', 'Ranson', 'ССВР', 'None', 'М-APACHE II', 'к/день в ОАРИТ', '№сутки в ОАРИТ', 'нач.заб.,час', 'тип госпитализации', 'Сутки перевода', '№ госп-и', 'ранн.опер.', 'расп.боли', 'нарк.анальг.', 'мн.рвота', 'рв.коф.гущ.', 'BMI', 'c-м Грея-К.', 'Группа крови', 'Rh', 'ЭКГ', 'None', 'эффект от леч. 24 часа', 'антибак.проф.', 'антиферм.', 'антисекр.преп.', 'гемосорб.', 'СДК', 'нутр.подд-ка.', 'дрен.УЗ-контр', 'лап.(леч.)', 'ЭПСТ', 'ПКТ', 'ТИАБ(диагн)', 'миниинваз.оп.', 'лапаротомий-всего', 'Примечание', 'ОЦЕНКА',]
+var periodInformationArray = ['темп.тела', 'част.пульса', 'АДсист.', 'вазопресс.', 'част. дых.', 'сатурация', 'ИВЛ', 'диурез,мл', 'сознание', 'зонд,мл', 'вздутие ж.', 'ОЖО или св.ж.', 'ЦВД,мм вод.', 'лапарос.диагн.', 'ЭФГДС', 'УЗИ', 'КТ', 'посев крови', 'посев ОЖО', 'имм.статус', 'эритр.', 'Hb', 'Ht', 'СОЭ', 'лейк.', 'тромбоциты, кол-во', 'тромбоциты, объем', 'п/ядерн.', 'лимфоциты', 'нейтрофилы', 'ЛИИ', 'глюкоза', 'белок', 'АлАТ', 'АсАТ,', 'билир.', 'ЩФ', 'ГГТП', 'мочевина', 'креатинин', 'амилаза мочи', 'амилаза крови', 'кальций', 'ЛДГ', 'СРБ', 'рН', 'рСО2', 'рО2', 'НСО3', 'tCO2', 'ABE', 'SO2', 'АЧТВ', 'ПИ', 'МНО', 'PROMISE',]
+transliterate = (
+	function() {
+		var
+			rus = "щ   ш  ч  ц  ю  я  ё  ж  ъ  ы  э  а б в г д е з и й к л м н о п р с т у ф х ь".split(/ +/g),
+			eng = "shh sh ch cz yu ya yo zh `` y' e` a b v g d e z i j k l m n o p r s t u f x `".split(/ +/g)
+		;
+		return function(text, engToRus) {
+			var x;
+			for(x = 0; x < rus.length; x++) {
+				text = text.split(engToRus ? eng[x] : rus[x]).join(engToRus ? rus[x] : eng[x]);
+				text = text.split(engToRus ? eng[x].toUpperCase() : rus[x].toUpperCase()).join(engToRus ? rus[x].toUpperCase() : eng[x].toUpperCase());	
+			}
+			return text;
+		}
+	}
+)();
